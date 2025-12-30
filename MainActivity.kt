9a85370f68a6674551b62029dcc4d95210d9469b
xthan <hackattendant@yahoo.com>
2025-12-30 01:26:00 -0500

    whoops wrong package name hellongoobyenworlds

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hgworlds
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+
+        val webV: WebView = findViewById(R.id.myWebView)
+        webV.settings.javaScriptEnabled = true
+        webV.loadUrl("file:///android_asset/index.html")
+
     }
+
+
 }
